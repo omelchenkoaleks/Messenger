482054144da1597e8099108124558cc45aff15a4
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-06-24 07:11:29 +0300

    Initial commit

---
+++ app/src/main/java/com/omelchenkoaleks/messenger/CreateMessageActivity.java
@@ -0,0 +1,14 @@
+package com.omelchenkoaleks.messenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+}
