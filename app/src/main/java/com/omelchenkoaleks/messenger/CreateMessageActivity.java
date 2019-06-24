d36c95061171d4a5abe7bcf0d637c2a2dcab8ed8
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-06-24 07:53:48 +0300

    взаимодействие двух активити через интент - передача данных ...

--- app/src/main/java/com/omelchenkoaleks/messenger/CreateMessageActivity.java
+++ app/src/main/java/com/omelchenkoaleks/messenger/CreateMessageActivity.java
@@ -2,18 +2,26 @@ package com.omelchenkoaleks.messenger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class CreateMessageActivity extends AppCompatActivity {
+    private EditText mMessageEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_message);
+
+        mMessageEditText = findViewById(R.id.message_edit_text);
     }
 
     public void onClickSendMessage(View view) {
-
+        String message = mMessageEditText.getText().toString().trim();
+        Intent intent = new Intent(this, ReceivedMassageActivity.class);
+        intent.putExtra("msg", message);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/omelchenkoaleks/messenger/ReceivedMassageActivity.java
+++ app/src/main/java/com/omelchenkoaleks/messenger/ReceivedMassageActivity.java
@@ -2,13 +2,24 @@ package com.omelchenkoaleks.messenger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ReceivedMassageActivity extends AppCompatActivity {
+    private TextView mReceivedMessageTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_received_massage);
+
+        mReceivedMessageTextView = findViewById(R.id.received_message_text_view);
+
+        Intent intent = getIntent();
+        // сохраняем полученные данные из интента в переменную
+        String message = intent.getStringExtra("msg");
+
+        mReceivedMessageTextView.setText(message);
     }
 }
