bedcdc519d3e5e0d7abd2f986b535d17799f3f3c
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-06-24 07:29:27 +0300

    подготовлены две активности для будущего взаимодействия между собой ...

--- app/src/main/java/com/omelchenkoaleks/messenger/ReceivedMassageActivity.java
+++ app/src/main/java/com/omelchenkoaleks/messenger/ReceivedMassageActivity.java
@@ -3,17 +3,12 @@ package com.omelchenkoaleks.messenger;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class CreateMessageActivity extends AppCompatActivity {
+public class ReceivedMassageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_message);
-    }
-
-    public void onClickSendMessage(View view) {
-
+        setContentView(R.layout.activity_received_massage);
     }
 }
